d19a2a8387bd7546911d6a52f1c7c1c475d99fcc
Chipo <41240403+cmapimha@users.noreply.github.com>
2026-07-16 21:49:21 +0000

    add hello world app code

---
+++ HelloWorld.cs
@@ -0,0 +1,7 @@
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+Console.Write($"{Environment.NewLine}Press Enter to exit...");
+Console.Read();
